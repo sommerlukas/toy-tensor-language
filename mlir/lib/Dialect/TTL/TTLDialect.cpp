a3b5afc2a84f610fdfd77e3b7edf7442174817aa
Lukas Sommer <lukas.sommer@codeplay.com>
2025-06-10 17:58:46 +0200

    Implement inliner interface (#10)
    Implement the dialect interface for the inliner to enable inlining of function calls in TTL programs.
    
    ---------
    
    Signed-off-by: Lukas Sommer <lukas.sommer@codeplay.com>

--- mlir/lib/Dialect/TTL/TTLDialect.cpp
+++ mlir/lib/Dialect/TTL/TTLDialect.cpp
@@ -5,6 +5,7 @@
 #include "mlir/Bytecode/BytecodeOpInterface.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/DialectImplementation.h"
+#include "mlir/Transforms/InliningUtils.h"
 #include "llvm/ADT/TypeSwitch.h"
 
 using namespace mlir;
@@ -12,12 +13,37 @@ using namespace mlir::ttl;
 
 #include "Dialect/TTL/TTLOpsDialect.cpp.inc"
 
+namespace {
+struct TTLInlinerInterface : public DialectInlinerInterface {
+  using DialectInlinerInterface::DialectInlinerInterface;
+
+  bool isLegalToInline(Region *, Region *, bool, IRMapping &) const final {
+    return true;
+  }
+
+  bool isLegalToInline(Operation *, Region *, bool, IRMapping &) const final {
+    return true;
+  }
+
+  void handleTerminator(Operation *op, ValueRange valuesToRepl) const final {
+    if (!isa<ttl::Yield>(op) && !isa<ttl::Return>(op)) {
+      return;
+    }
+
+    for (auto retValue : llvm::zip_equal(valuesToRepl, op->getOperands())) {
+      std::get<0>(retValue).replaceAllUsesWith(std::get<1>(retValue));
+    }
+  }
+};
+} // namespace
+
 void TTLDialect::initialize() {
   addOperations<
 #define GET_OP_LIST
 #include "Dialect/TTL/TTLOps.cpp.inc"
       >();
   registerTypes();
+  addInterfaces<TTLInlinerInterface>();
 }
 
 Operation *TTLDialect::materializeConstant(OpBuilder &builder, Attribute value,
